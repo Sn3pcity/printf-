89e9a417ecb2fb77d525dd0c16e34ccb5fb297cf
Snap <113688601+Sn3pcity@users.noreply.github.com>
2022-11-25 00:11:03 +0300

    Update main.h

--- main.h
+++ main.h
@@ -4,4 +4,10 @@
 #include <stdio.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
 #endif
